8206f27ffd509c095471ce4e2f0f8f36cb2b6ba0
Francisco <fnaveira11@gmail.com>
2021-04-02 13:44:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
+const { returns } = require("chai-spies")
+
 /* Your Code Here */
+function createEmployeeRecord(employeeArry) {
+    const employeeObject = {firstName: employeeArry[0], familyName: employeeArry[1], title: employeeArry[2], payPerHour: employeeArry[3], timeInEvents: [], timeOutEvents: []}
+    return employeeObject
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(arryOfEmployeeArrys) {
+    return arryOfEmployeeArrys.map( e => createEmployeeRecord(e))
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(timeStamp) {
+    let stampSplit = timeStamp.split(' ')
+    let date = stampSplit[0] 
+    let hour = stampSplit[1] 
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date,
+    })
+    return this
+}
+
+function createTimeOutEvent(timeStamp) {
+    let stampSplit = timeStamp.split(' ')
+    let date = stampSplit[0] 
+    let hour = stampSplit[1] 
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(formDate) {
+    let timeInArry = this.timeInEvents
+    let timeOutArry = this.timeOutEvents
+    let timeInObject = timeInArry.find(e => e.date === formDate)
+    let timeOutObject = timeOutArry.find(e => e.date === formDate)
+    return (timeOutObject.hour - timeInObject.hour)/100
+}
+
+function wagesEarnedOnDate(formDate) {
+    const wages = hoursWorkedOnDate.call(this, formDate) * this.payPerHour
+    return wages
+}
+
+
+
+function findEmployeeByFirstName(srcArry, stringName) {
+    return srcArry.find(e => e.firstName === stringName)
+}
+
+function calculatePayroll(arry) {
+    return arry.reduce((memo, e)=>{
+        return memo += allWagesFor(e)
+    },0)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
